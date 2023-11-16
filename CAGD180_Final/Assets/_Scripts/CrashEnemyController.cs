692389ce79446ba519acae634d8b15eac0cabf83
EJJ143 <tankcommander123@gmail.com>
2023-11-16 08:54:24 -0800

    Crash Enemy Working

--- CAGD180_Final/Assets/_Scripts/CrashEnemyController.cs
+++ CAGD180_Final/Assets/_Scripts/CrashEnemyController.cs
@@ -4,37 +4,56 @@ using UnityEngine;
 
 public class CrashEnemyController : MonoBehaviour
 {
-    private bool waiting;
+    private bool waiting = false;
     public float speed;
+    private int count;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement(); 
+       
+        
+        Attack();
     }
 
-    private void Movement()
+
+    private void Attack()
     {
-        if (transform.position.y <= -6)
+        if (waiting == false)
         {
-            transform.Rotate(0f, 0f, 180f);
+
+
             transform.position += -transform.up * speed * Time.deltaTime;
+            if (transform.position.y <= -5)
+            {
+                transform.Rotate(0f, 0f, 180f);
+
+            }
+            if (transform.position.y >= 6)
+            {
+                transform.Rotate(0f, 0f, 180f);
+                count++;
+            }
+            if (count == 2)
+            {
+                StartCoroutine(Wait());
+            }
+
+
         }
-        if (transform.position.y >= 6)
-        {
-            transform.Rotate(0f, 0f, 180f);
-            transform.position += -transform.up * speed * Time.deltaTime;
-        }
-        
     }
-    
-    private IEnumerator Attacking()
+    private IEnumerator Wait()
     {
+        Debug.Log("wait started");
+        waiting = true;
         yield return new WaitForSeconds(5f);
+        waiting = false;
+        Debug.Log("wait ended");
+        count = 0;
     }
 }
